8c7425b37d7278d491cf4dadc6af9b84fbb17309
fennec.fox <fennec.klar@kakaomobility.com>
2020-09-21 09:15:14 +0900

    Init project

---
+++ build.gradle.kts
@@ -0,0 +1,99 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    java
+    idea
+    id("org.springframework.boot") version "2.3.4.RELEASE"
+    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    id("com.avast.gradle.docker-compose") version "0.13.2"
+    kotlin("jvm") version "1.3.71"
+    kotlin("plugin.spring") version "1.3.71"
+    kotlin("plugin.allopen") version "1.3.71"
+    id("org.jmailen.kotlinter") version "3.0.2"
+}
+
+group = "io.mustelidae"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+    mavenLocal()
+    jcenter()
+    maven("https://palantir.bintray.com/releases/")
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
+    implementation("com.github.kittinunf.fuel:fuel:2.2.3")
+
+    testImplementation("cz.jirutka.spring:embedmongo-spring:1.3.1")
+    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo:2.2.0")
+
+    testImplementation("io.mockk:mockk:1.9.3")
+    implementation("io.springfox:springfox-swagger2:3.0.0")
+    implementation("io.springfox:springfox-swagger-ui:3.0.0")
+    implementation("io.springfox:springfox-bean-validators:3.0.0")
+    testImplementation("io.mockk:mockk:1.10.0")
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
+
+    implementation("jakarta.persistence:jakarta.persistence-api:2.2.3")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot:spring-boot-starter-web") {
+        exclude(module = "spring-boot-starter-tomcat")
+    }
+    implementation("org.springframework.boot:spring-boot-starter-undertow") {
+        exclude("io.undertow", "undertow-websockets-jsr")
+    }
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+        exclude(group = "junit", module = "junit")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+
+    addTestListener(object : TestListener {
+        override fun beforeSuite(suite: TestDescriptor) {}
+        override fun beforeTest(testDescriptor: TestDescriptor) {}
+        override fun afterTest(testDescriptor: TestDescriptor, result: TestResult) {}
+        override fun afterSuite(suite: TestDescriptor, result: TestResult) {
+            if (suite.parent == null) {
+                val output =
+                    "Results: ${result.resultType} (${result.testCount} tests, ${result.successfulTestCount} successes, ${result.failedTestCount} failures, ${result.skippedTestCount} skipped)"
+                val startItem = "|  "
+                val endItem = "  |"
+                val repeatLength = startItem.length + output.length + endItem.length
+                println("\n${"-".repeat(repeatLength)}\n|  $output  |\n${"-".repeat(repeatLength)}")
+            }
+        }
+    })
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
+}
+
+tasks.register("version") {
+    println(version)
+}
+
+dockerCompose {
+// docker-compose
+    // settings as usual
+    createNested("infraSetting").apply {
+        stopContainers = false
+        useComposeFiles = listOf("docker-compose.yml")
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "river-otter"
+
