f2af0d56f99e435e30e0b18357d94ea5cb108d6e
fennec.fox <fennec.klar@kakaomobility.com>
2021-06-09 18:29:44 +0900

    Remove Client handler

--- src/main/kotlin/io/mustelidae/riverotter/domain/calendar/holiday/HolidayCrawler.kt
+++ src/main/kotlin/io/mustelidae/riverotter/domain/calendar/holiday/HolidayCrawler.kt
@@ -5,20 +5,19 @@ import io.mustelidae.riverotter.config.NotSupportCountryException
 import io.mustelidae.riverotter.domain.calendar.holiday.country.KoreaHoliday
 import io.mustelidae.riverotter.domain.calendar.holiday.country.UnitedStateHoliday
 import io.mustelidae.riverotter.domain.calendar.holiday.repository.HolidayCalendarRepository
-import io.mustelidae.riverotter.domain.client.ClientHandler
+import io.mustelidae.riverotter.domain.client.abstractapi.WorldHolidayClient
+import io.mustelidae.riverotter.domain.client.korea.government.GovernmentOpenClient
 import org.bson.types.ObjectId
 import org.springframework.stereotype.Service
 import java.util.Locale
 
 @Service
 class HolidayCrawler(
-    clientHandler: ClientHandler,
     private val env: AppEnvironment,
+    private val governmentOpenClient: GovernmentOpenClient,
+    private val worldHolidayClient: WorldHolidayClient,
     private val holidayCalendarRepository: HolidayCalendarRepository
 ) {
-    private val governmentOpenClient = clientHandler.governmentOpenClient()
-    private val worldHolidayClient = clientHandler.worldHolidayClient()
-
     fun crawling(year: Int, country: Locale): ObjectId {
         val countryHoliday = when (country) {
             Locale.KOREA -> KoreaHoliday(env.country.korea, governmentOpenClient, holidayCalendarRepository)
