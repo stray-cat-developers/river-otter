33095256f90aefb2e513df326775c1c2ed30f466
fennec-fox <elijah17@gmail.com>
2021-12-26 15:08:25 +0900

    Add DirtiesContext

--- src/test/kotlin/io/mustelidae/riverotter/domain/config/IntegrationSupport.kt
+++ src/test/kotlin/io/mustelidae/riverotter/domain/config/IntegrationSupport.kt
@@ -5,9 +5,11 @@ import io.mustelidae.riverotter.config.SwaggerConfiguration
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 
 @ActiveProfiles("embedded")
+@DirtiesContext
 @ComponentScan(excludeFilters = [ComponentScan.Filter(classes = [SwaggerConfiguration::class])])
 @SpringBootTest(classes = [RiverOtterApplication::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class IntegrationSupport {
